aaed9874a656cf8eafef46b9bce2c1353bb3cfd6
Marcelo Ragnelli Frigerio <mrfrigerio@yahoo.com.br>
2023-10-12 22:23:24 -0300

    Criados os endpoints de CRUD de Generos via CBV do DRF

--- genres/views.py
+++ genres/views.py
@@ -3,8 +3,20 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics
 
 from genres.models import Genre
+from genres.serializers import GenreSerializer
+
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
 
 # Create your views here.
